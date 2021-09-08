fca6923872d04e9a63d7f4c512eed9e25769bab9
saraAAZ <sara.abu.zena2000@gmail.com>
2021-09-08 23:44:11 +0300

    React HW Solution

--- secondapp/src/App.js
+++ secondapp/src/App.js
@@ -2,24 +2,94 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  let text1=' Hello Palestine'
-  let text2='Hello World'
-  const [text, setText]= useState(text2);
-  const changeText = () =>{
-    if(text === text1){
-      setText(text2);
+    let text1 = ' Hello Palestine'
+    let text2 = 'Hello World'
+    const [text, setText] = useState(text2);
+    const changeText = () => {
+        if (text === text1) {
+            setText(text2);
+        } else {
+            setText(text1);
+        }
     }
-    else{
-      setText(text1);
+
+    let newEmail = "";
+    let newUserName = "";
+    const [userName, setUserName] = useState(newUserName);
+    const [email, setEmail] = useState(newEmail);
+    const saveEmail = (event) => {
+        if (event.target.value.trim("") != "") {
+            console.log(event.target.value)
+            setEmail(event.target.value);
+        }
+        if (event.target.value.trim("") == "") {
+            setEmail("");
+        }
     }
-  }
-  return (
-    <div> 
-      <div onClick ={changeText}>
-        {text} 
-      </div>
-    </div>
-  );
+    const saveUserName = (event) => {
+        if (event.target.value.trim("") != "") {
+            console.log(event.target.value)
+            setUserName(event.target.value);
+        }
+        if (event.target.value.trim("") == "") {
+            setUserName("");
+        }
+
+    }
+    const randomArray = [
+        '	He struck a match.',
+        '	I have got to leave here early tomorrow morning.',
+        'Ill have a glass of water pleas',
+        'Dont let your feelings show.',
+        'This book contains many pictures.	',
+        'Ill come by 10.	',
+        'She might come.	',
+        'He is sick.	',
+        'Are you ready?	,',
+        'I want to climb Mt. Fuji again.	',
+        'Its easy.	',
+        'Did you buy anything?	',
+        'What are you going to have?	',
+        'She lives with him in a small apartment.	'
+    ];
+    const [randomSentence, setrandomSentence] = useState("Change Sentence");
+    const changeSentence = () => {
+        let number = Math.floor(Math.random() * 100);
+        while (number > randomArray.length || number < 0) {
+            number = Math.floor(Math.random() * 10);
+        }
+        console.log(number)
+        console.log(randomArray[number])
+        setrandomSentence(randomArray[number])
+
+    }
+    return (
+        <div>
+            <div onClick ={changeText}>
+                <br></br>
+                {text} </div>
+            <hr></hr>
+            <div>
+                <label>User Name : {userName} </label>
+                <br></br>
+                <input type="text" placeholder="User Name"
+                    onChange={saveUserName}></input>
+                <br></br>
+                <label>Email : {email} </label>
+                <br></br>
+                <input type="email" placeholder="Email"
+                    onChange={saveEmail}></input>
+                <br></br>
+                <br></br>
+                <hr></hr>
+            </div>
+            <div>
+                <br></br>
+                <button onClick={changeSentence}>
+                    {randomSentence}</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
